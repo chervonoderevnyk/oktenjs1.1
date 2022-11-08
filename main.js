c4d25d385fa04d7c7de0600d923be789e3cb43ad
bigbird <igorchornyj@gmail.com>
2022-11-08 17:19:10 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,56 @@
+let a = `hello`;
+let b = `owu`;
+let c = `com`;
+let d = `ua`;
+let e = `1`;
+let f = `10`;
+let g = `-999`;
+let h = `123`;
+let j = `3.14`;
+let k = `2.7`;
+let l = `16`;
+let m = `true`;
+let n = `false`;
+console.log(a, b, c, d, e, f, g, h, j, k, l, m, n);
+
+let o = `Chornyj `;
+let p = `Ihor `;
+let q = `Petrovych`;
+let qaqa = o + p + q;
+console.log(qaqa);
+
+let r = true;
+let s = `100`;
+let t = `'100'`;
+console.log(r, s, t);
+
+let arr;
+arr = [343, 7, 425, 900, 404, 3, 45423, 555, 66, 888];
+console.log(arr);
+arr = [];
+arr[0] = 1;
+arr[1] = `he`;
+arr[10] = true;
+arr[5] = `world`;
+console.log(arr);
+console.log([3]);
+
+let library1 = [
+    {id: 1, name: `europa`, page: 500, genre: `psykhology`, writer : [ `Komarek`, 64]},
+    {id: 2, name: `animal farm`, page: 185, genre: `story`, writer : [ `Orwel`, `died`]},
+    {id: 3, name: `little prince`, page: 110, genre: `novel`, writer : [ `Saint Exupery`, `died`]}
+];
+console.log(library1);
+
+let library2 = [
+    {id: 1, name: `europa`, page: 500, genre: `psykhology`, writer : [ `Komarek`, 64]},
+    {id: 2, name: `animal farm`, page: 185, genre: `story`, writer : [ `Orwel`, `died`]},
+    {id: 3, name: `little prince`, page: 110, genre: `novel`, writer : [ `Saint Exupery`, `died`]}
+];
+console.log(library2);
+console.log(library2[0]);
+console.log(library2[0].writer);
+console.log(library2[0].writer[0]);
+console.log(library2[0].writer[1]);
+
+
